92f904d60cc8a33fbe6be950c1a1e147f43e76e1
DavidSiawutey <davidsiawutey1245@gmail.com>
2026-09-22 18:12:30 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Welcome message
+console.log("Welcome to Cheatcodes Portfolio 🚀");
+
+// Hire Me button
+const hireButton = document.querySelector(".button");
+
+hireButton.addEventListener("click", function () {
+    console.log("Someone clicked Hire Me!");
+});
